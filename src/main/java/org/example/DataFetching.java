611197b46be484c7fb27cbe4b1a61473d479d4c8
Ajeet Verma <av401402@gmail.com>
2025-04-12 09:27:25 +0530

    Data is getting fetched

--- src/main/java/org/example/DataFetching.java
+++ src/main/java/org/example/DataFetching.java
@@ -7,7 +7,9 @@ import org.hibernate.cfg.Configuration;
 public class DataFetching {
 
     public static void main(String[] args) {
-        Configuration cfg = new Configuration().configure();
+        Configuration cfg = new Configuration().configure(); // If hibernate.cfg.xml file is not at the resources
+        // folder then provide full path
+//        cfg.configure();
         SessionFactory sFac = cfg.buildSessionFactory();
         Session session = sFac.openSession();
         Student s1  = (Student)session.get( Student.class,1);
