412c250ea18299d2a5fa5151adb72d5a4bfb1090
Ajeet Verma <av401402@gmail.com>
2025-04-14 10:54:32 +0530

    ques class for one to one mapping

---
+++ src/main/java/com/mapping/Question.java
@@ -0,0 +1,37 @@
+package com.mapping;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Question {
+    @Id
+    int quesId;
+    String ques;
+
+    public Question(int quesId, String ques) {
+        super();
+        this.quesId = quesId;
+        this.ques = ques;
+    }
+
+    public Question() {
+        super();
+    }
+
+    public int getQuesId() {
+        return quesId;
+    }
+
+    public void setQuesId(int quesId) {
+        this.quesId = quesId;
+    }
+
+    public String getQues() {
+        return ques;
+    }
+
+    public void setQues(String ques) {
+        this.ques = ques;
+    }
+}
