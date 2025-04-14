5df46c28e0d46b4dd9b0e4f09735e29e198c173c
Ajeet Verma <av401402@gmail.com>
2025-04-14 10:58:47 +0530

    basic code for one to one

--- src/main/java/com/mapping/LearningMapping.java
+++ src/main/java/com/mapping/LearningMapping.java
@@ -1,4 +1,23 @@
 package com.mapping;
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class LearningMapping {
+    public static void main(String[] args) {
+
+        Configuration cfg = new Configuration().configure();
+        SessionFactory sFac = cfg.buildSessionFactory();
+        Session session = sFac.openSession();
+        Transaction tr = session.beginTransaction();
+
+
+
+        tr.commit();
+        sFac.close();
+        session.close();
+    }
 }
