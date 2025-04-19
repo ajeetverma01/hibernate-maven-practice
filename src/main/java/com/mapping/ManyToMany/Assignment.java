3bb09bb6f53758d9ca3a121814daf5f52aa83ddb
Ajeet Verma <av401402@gmail.com>
2025-04-19 20:14:35 +0530

    classes created

--- src/main/java/com/mapping/ManyToMany/Assignment.java
+++ src/main/java/com/mapping/ManyToMany/Assignment.java
@@ -1,4 +1,30 @@
 package com.mapping.ManyToMany;
 
 public class Assignment {
+    private int aId;
+    private String aName;
+
+    public Assignment(int aId, String aName) {
+        this.aId = aId;
+        this.aName = aName;
+    }
+
+    public Assignment() {
+    }
+
+    public int getaId() {
+        return aId;
+    }
+
+    public void setaId(int aId) {
+        this.aId = aId;
+    }
+
+    public String getaName() {
+        return aName;
+    }
+
+    public void setaName(String aName) {
+        this.aName = aName;
+    }
 }
--- src/main/java/com/mapping/ManyToMany/Employee.java
+++ src/main/java/com/mapping/ManyToMany/Employee.java
@@ -1,4 +1,32 @@
 package com.mapping.ManyToMany;
 
 public class Employee {
+    private int eId;
+
+    public Employee(int eId, String eName) {
+        this.eId = eId;
+        this.eName = eName;
+    }
+
+    public Employee() {
+        super();
+    }
+
+    public int geteId() {
+        return eId;
+    }
+
+    public void seteId(int eId) {
+        this.eId = eId;
+    }
+
+    public String geteName() {
+        return eName;
+    }
+
+    public void seteName(String eName) {
+        this.eName = eName;
+    }
+
+    private String eName;
 }
