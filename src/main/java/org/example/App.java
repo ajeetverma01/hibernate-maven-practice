49f49ca83eabf809596dc38d35fd3195671d2c22
Ajeet Verma <av401402@gmail.com>
2025-05-07 11:13:06 +0530

    Inserting the data

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -22,18 +22,22 @@ public class App
         Transaction tr =session.beginTransaction();
 
 
-        Student s= new Student(2, "John", "uk");
+        Student s= new Student(21, "Rah", "uk");
+        Student s2= new Student(63, "Sah", "gonda");
+        Certificate c1 = new Certificate("BCA","3 year");
+        s.setC1(c1);
+        s2.setC1(c1);
 //        session.save(s);  //The save() method is deprecated.
         session.persist(s); //can be used for saving.
-
-        FileInputStream fis = new FileInputStream("src/main/java/a.png");
-        byte[] img = new byte[fis.available()];
-        fis.read(img);
-        InsertingImage i1=new InsertingImage(1,"Ajeet", img);
-        session.persist(i1);
-
-        LearningAnnotations l1 = new LearningAnnotations(1,"gonda","city", true,new Date(), 1.5, img);
-        session.save(l1);
+        session.persist(s2);
+//        FileInputStream fis = new FileInputStream("src/main/java/a.png");
+//        byte[] img = new byte[fis.available()];
+//        fis.read(img);
+//        InsertingImage i1=new InsertingImage(1,"Ajeet", img);
+//        session.persist(i1);
+//
+//        LearningAnnotations l1 = new LearningAnnotations(1,"gonda","city", true,new Date(), 1.5, img);
+//        session.save(l1);
 
 
 
