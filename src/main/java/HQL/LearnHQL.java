14dc99cf47528615c2970deba21adda8108035fd
Ajeet Verma <av401402@gmail.com>
2025-05-07 11:02:12 +0530

    Basic classes and objects created and instantiated

--- src/main/java/HQL/LearnHQL.java
+++ src/main/java/HQL/LearnHQL.java
@@ -1,5 +1,21 @@
 package HQL;
 
-public class LearnHQL {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+public class LearnHQL {
+    public static void main(String[] args) {
+        Configuration cfg = new Configuration();
+        cfg.configure();
+        SessionFactory sFac = cfg.buildSessionFactory();
+        Session session = sFac.openSession();
+
+
+
+
+
+        sFac.close();
+        session.close();
+    }
 }
