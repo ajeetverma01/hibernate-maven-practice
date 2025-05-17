245085a9c8e74f3f826511c8a3d60daf3d64a8b3
Ajeet Verma <av401402@gmail.com>
2025-05-17 21:55:01 +0530

    necessary improts

--- src/main/java/Pagination/LearnPagination.java
+++ src/main/java/Pagination/LearnPagination.java
@@ -1,4 +1,13 @@
 package Pagination;
 
+import org.example.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
 public class LearnPagination {
+    public static void main(String[] args) {
+
+    }
 }
