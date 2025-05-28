91c6cfd619bf386735e883379e42760165e6b8c3
Ajeet Verma <av401402@gmail.com>
2025-05-28 15:50:19 +0530

    cascading

--- src/main/java/Cascading/CascadingEx.java
+++ src/main/java/Cascading/CascadingEx.java
@@ -5,7 +5,9 @@ import org.hibernate.cfg.Configuration;
 
 public class CascadingEx {
     public static void main(String[] args) {
-        SessionFactory sFac = new Configuration().configure().buildSessionFactory();
+        Configuration cfg = new Configuration();
+        cfg.configure();
+        SessionFactory sFac = cfg.buildSessionFactory();
 
     }
 }
