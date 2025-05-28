f5039f7e3549a421c75bc72e3cb0ba7c5572c986
Ajeet Verma <av401402@gmail.com>
2025-05-28 15:52:31 +0530

    q and a created

--- src/main/java/Cascading/CascadingEx.java
+++ src/main/java/Cascading/CascadingEx.java
@@ -1,15 +1,21 @@
 package Cascading;
 
+import com.OneToMany.Answer;
+import com.OneToMany.Question;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
 public class CascadingEx {
     public static void main(String[] args) {
         Configuration cfg = new Configuration();
         cfg.configure();
         SessionFactory sFac = cfg.buildSessionFactory();
         Session s = sFac.openSession();
+
 
         Question q1 = new Question();
         q1.setQuesId(5);
