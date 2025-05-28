bbc3642b50930a9a563987a0aba0607c89bcd260
Ajeet Verma <av401402@gmail.com>
2025-05-28 15:50:58 +0530

    session opened and factory created"

--- src/main/java/Cascading/CascadingEx.java
+++ src/main/java/Cascading/CascadingEx.java
@@ -1,5 +1,6 @@
 package Cascading;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -8,6 +9,6 @@ public class CascadingEx {
         Configuration cfg = new Configuration();
         cfg.configure();
         SessionFactory sFac = cfg.buildSessionFactory();
-
+        Session s = sFac.openSession();
     }
 }
