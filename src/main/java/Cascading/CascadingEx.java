fff72d2735c648150fa3a7aff3f1c5a194e98525
Ajeet Verma <av401402@gmail.com>
2025-05-28 15:51:27 +0530

    q and a created

--- src/main/java/Cascading/CascadingEx.java
+++ src/main/java/Cascading/CascadingEx.java
@@ -10,5 +10,14 @@ public class CascadingEx {
         cfg.configure();
         SessionFactory sFac = cfg.buildSessionFactory();
         Session s = sFac.openSession();
+
+        Question q1 = new Question();
+        q1.setQuesId(5);
+        q1.setQues("What is cascading");
+
+        Answer a1 = new Answer(1111, "imp concept");
+        Answer a2 = new Answer(2222, "2 concept");
+        Answer a3 = new Answer(2333, "3 concept");
+
     }
 }
