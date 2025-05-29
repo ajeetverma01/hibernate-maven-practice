9dfec0b3ed02040ffe739bea547e04b7e4db3c8f
Ajeet Verma <av401402@gmail.com>
2025-05-29 10:47:23 +0530

    session created and closed

--- src/main/java/Caching/FirstLevelEx.java
+++ src/main/java/Caching/FirstLevelEx.java
@@ -1,4 +1,24 @@
 package Caching;
 
+import org.example.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class FirstLevelEx {
+    public static void main(String[] args) {
+        SessionFactory sFac = new Configuration().configure().buildSessionFactory();
+        Session s = sFac.openSession();
+
+
+
+
+
+
+        Transaction t = s.beginTransaction();
+        t.commit();
+        sFac.close();
+        s.close();
+    }
 }
