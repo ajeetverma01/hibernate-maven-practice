faf333bc3b75defa617532497032f0c863afe493
Ajeet Verma <av401402@gmail.com>
2025-06-22 06:31:06 +0530

    Completed hbernater

--- src/main/java/Last.java
+++ src/main/java/Last.java
@@ -1,6 +1,6 @@
 public class Last {
     void context(){
-        System.out.println();
+        System.out.println("Completed hibernate learning");
     }
     public static void main(String[] args) {
 
